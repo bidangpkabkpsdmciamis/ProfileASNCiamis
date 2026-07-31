d49c92bcb74f2552ce61be210c612c7fc6f938f3
bidangpkabkpsdmciamis <bidangpkabkpsdmciamis@gmail.com>
2026-07-31 11:21:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // Jangan hardcode di file ini untuk keamanan
 const CONFIG = {
   // Untuk development, set manual. Untuk production, gunakan window.GAS_URL
-  GAS_URL: window.GAS_URL || 'https://script.google.com/macros/s/AKfycbzG2wcJT49KxrD7kLsFdbywiluBl_ij-wr9YLOkrhNClPzhOio_F1eqCsR1414CFTIQNA/exec',
+  GAS_URL: window.GAS_URL || 'https://script.google.com/macros/s/AKfycbz0Gxvtrf5j4gZrKimdMAnde8wmrQiALmdURC65cAsIQJhxicd-xn_2e_8Mp8eV0ZtM6Q/exec',
   NIP: '' // Akan diisi dari localStorage
 };
 
