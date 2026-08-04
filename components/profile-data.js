548606da142b6e9dff0e3feb80cdc906b6ea353e
bidangpkabkpsdmciamis <bidangpkabkpsdmciamis@gmail.com>
2026-08-04 11:48:23 +0700

    Update profile-data.js

--- components/profile-data.js
+++ components/profile-data.js
@@ -38,7 +38,7 @@ class ProfileDataAPI {
     }
   }
 
-  // ===== GET REKAP KOMPETENSI (DIPERBARUI) =====
+  // ===== GET REKAP KOMPETENSI =====
   async getRekapKompetensi(tahun = 'all') {
     if (!this.nip) {
       throw new Error('NIP tidak ditemukan');
@@ -60,7 +60,6 @@ class ProfileDataAPI {
       if (data && data.data) {
         data.data = data.data.map(item => ({
           ...item,
-          // Support berbagai kemungkinan nama kolom
           Link_Sertifikat: item.Link_Sertifikat || item.link_sertifikat || item['Link Sertifikat'] || item['link_sertifikat'] || '-'
         }));
       }
